f40ea9f5858b1837fa0a0d58bf746ee06f7d3b1f
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-28 23:00:04 +0100

    types in JS:

---
+++ Colt_Steele_Udemy/primitive_types/javascriptNumbers/README.md
@@ -0,0 +1,5 @@
+*JS has one number type
+*positive numbers
+*negative numbers
+*whole number(integers)
+*decimal numbers
---
+++ Colt_Steele_Udemy/primitive_types/javascriptNumbers/num.js
@@ -0,0 +1,13 @@
+let num1 = 50;
+let num2 = 7;
+let num3 = 3.874;
+let num4 = 0.99;
+let num5 = -45;
+let num6 = -777.23444;
+//printing output type for each variable 
+console.log(typeof(num1)); //number
+console.log(typeof(num2));  
+console.log(typeof(num3));
+console.log(typeof(num4));
+console.log(typeof(num5));
+console.log(typeof(num6));
