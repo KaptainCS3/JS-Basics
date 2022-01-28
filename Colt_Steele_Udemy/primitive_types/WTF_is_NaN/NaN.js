dd74849e1b394f893a0d8263cb202abc9d33dae6
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-28 23:11:06 +0100

    NaN concept done

---
+++ Colt_Steele_Udemy/primitive_types/WTF_is_NaN/NaN.js
@@ -0,0 +1,9 @@
+// its a concept for representing values of mathematical operations with are not number
+// its consider a number in JS :( 
+    //example of NaN
+    let result = 0 / 0;
+    let num = 1 + NaN;
+    console.log(result);  //result is NaN
+    console.log(typeof(result)); 
+    console.log(num);
+    console.log(typeof(num));
