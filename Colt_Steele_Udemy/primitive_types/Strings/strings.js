c19c199dfd3ecba6b1cb1da61c6f484c54137f92
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-29 08:04:38 +0100

    string intro down

---
+++ Colt_Steele_Udemy/primitive_types/Strings/strings.js
@@ -0,0 +1,10 @@
+
+    //done by KaptainCS3 29 /01/ 2022
+        
+            //remain consistent using single or double quotes.
+
+let userName = 'KaptainCS3';    //single or double quotes for representing stings
+let firstName = "Kim";
+let lastName = "Louis";
+let stat = "let's lead tech";   // can use *'* because string contains single quote character
+let favAnimal = "Bull Dog";
