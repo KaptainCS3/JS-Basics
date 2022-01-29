a1efacab490df4d5993df99c2039ceb0caa7ee70
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-29 08:44:56 +0100

    string V1

---
+++ Colt_Steele_Udemy/primitive_types/Strings/Indices_&_Length/stringV1.js
@@ -0,0 +1,10 @@
+
+    //done by KaptainCS3  29 /01/ 2022
+
+        //string properties and methods
+let today = 'Saturday'.length; // property
+let name = 'robert'.toLocaleUpperCase(); // method
+let greeting = '   good morning friends   '.trim(); // remove white space at the begining at end of a string
+console.log(today); // returns string's length
+console.log(name);
+console.log(greeting);
