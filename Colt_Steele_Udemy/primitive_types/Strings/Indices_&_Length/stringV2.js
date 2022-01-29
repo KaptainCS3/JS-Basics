c83765cbc16e5f888c57d220fcdaf8005c9790a7
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-29 09:24:18 +0100

    chain method down

---
+++ Colt_Steele_Udemy/primitive_types/Strings/Indices_&_Length/stringV2.js
@@ -0,0 +1,9 @@
+
+    //done by KaptainCS3  29 /01/ 2022
+
+// chaining methods
+let userInput = "   great website content   ".toLocaleUpperCase().trim();
+let psw = " WelcomeNewUser  ".toLowerCase().trim();
+const adr = "   Street 7 Califonia   ".toLocaleUpperCase().trim();
+
+
