60e442bafcda575ce6db287442b4ed0694fea2b4
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-30 22:37:20 +0100

    str methods covered

---
+++ Colt_Steele_Udemy/primitive_types/Strings/String_Methods/str_method.js
@@ -0,0 +1,7 @@
+
+    //done by KaptainCS3  30 /01/ 2022
+
+let name = 'robert'.toLocaleUpperCase(); // method
+let greeting = '   good morning friends   '.trim(); // remove white space at the begining at end of a string
+console.log(name);
+console.log(greeting);
