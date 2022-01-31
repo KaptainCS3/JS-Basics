1ab4d777d53d5e4b6823464a28f85577022fb335
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-01-31 20:11:09 +0100

    Update str_Arg.js
    Add include method similar to index of method

--- Colt_Steele_Udemy/primitive_types/Strings/String_Arguments/str_Arg.js
+++ Colt_Steele_Udemy/primitive_types/Strings/String_Arguments/str_Arg.js
@@ -4,6 +4,11 @@
 
 let day = "SunSunday".indexOf('Sun'); /* returns the index within the calling string object 
                                         of the first occurrence of the specific value */
+// For each through a string for first match
+alert( "Widget with id".includes("Widget") ); // true
+
+alert( "Hello".includes("Bye") ); // false
+
                                     
 let newStr = "Welcome to Paris".indexOf('A'); //when string not found method returns "-1"
 let slice = "i hate monday".slice(2,6) //methods extractsa section of  a string to retun it as a new string without modify the original "2" = starting index, "6" = ending index, but ending index is opitonal
