0cb5515a3914a371773e6c49b42a3d09358c7da5
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-07 21:51:37 +0100

    string templates

---
+++ Colt_Steele_Udemy/primitive_types/Strings/String_Template_Literals/Str_Tem.js
@@ -0,0 +1,13 @@
+
+    //  done by KaptainCS3  07/ 02 / 2022
+let age = 20;
+let name = 'KaptainCS3';
+const prize = '250frs';
+
+console.log(`Hey dude my name is ${name} and i'm ${age} years old or young what ever!!,`);
+confirm("you're welcome");
+console.log(`No need to tell your age`);
+confirm(`Thanks dude`);
+console.log(`How much do you sell this delicious bananas?`);
+console.log(`${prize}`);
+
