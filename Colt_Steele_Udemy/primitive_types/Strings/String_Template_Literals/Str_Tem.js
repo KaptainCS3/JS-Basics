502239269df9a4c616bc7f9f7a7f1eb2b43c357f
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-07 21:54:51 +0100

    minor changes

--- Colt_Steele_Udemy/primitive_types/Strings/String_Template_Literals/Str_Tem.js
+++ Colt_Steele_Udemy/primitive_types/Strings/String_Template_Literals/Str_Tem.js
@@ -4,6 +4,7 @@ let age = 20;
 let name = 'KaptainCS3';
 const prize = '250frs';
 
+// resolve issues of single and double quotes, easy to concatinate strings uisng the back tick for str literals
 console.log(`Hey dude my name is ${name} and i'm ${age} years old or young what ever!!,`);
 confirm("you're welcome");
 console.log(`No need to tell your age`);
