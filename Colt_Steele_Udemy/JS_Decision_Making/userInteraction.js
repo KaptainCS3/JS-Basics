dd720aee7c4afff304a1ea4f679e26f3bdcc65e4
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-13 14:44:05 +0100

    section covered

---
+++ Colt_Steele_Udemy/JS_Decision_Making/userInteraction.js
@@ -0,0 +1,27 @@
+//  done by KaptainCS3  13 /02/ 2022
+
+
+/*printing out string to the console
+- printing different form of messages 
+-console.error
+-console.warn
+-console.log
+
+*/ 
+console.log("Output a string");
+console.warn("Output a string");
+console.error("Output a string");
+
+/*collect input from user it might contain to parameters
+-first paramter for labeling input field
+-second for placeholder text
+*/
+let name = prompt("Enter your name", "Enter names all in Caps");
+
+
+/*printing out string to the screen through pop up */
+
+alert("Today is Sunday :)");
+
+/* return boolean pop up to the console*/
+confirm("Are you sure you want to quit!!");
