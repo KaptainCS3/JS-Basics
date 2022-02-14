d43d5adaf6fa48e505f47180f122511fd6b7ddcf
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-14 10:58:33 +0100

    rolldie

---
+++ Colt_Steele_Udemy/primitive_types/Strings/practice_Exercises_str/stringTemplate/rolldie.js
@@ -0,0 +1,9 @@
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+console.log(`die1: ${die1}`);
+console.log(`die2: ${die2}`);
+const roll = `roll: You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
+console.log(roll);
