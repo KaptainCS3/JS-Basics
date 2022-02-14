fcd2df79edaedc007e3796b3d3948a4852a28da6
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-14 11:15:32 +0100

    if condition section covered

---
+++ Colt_Steele_Udemy/JS_Decision_Making/ifCon.js
@@ -0,0 +1,11 @@
+// done by KaptainCS3   14 /02/ 2022
+//if condition with concept for all programming languages
+
+let rating = 3
+let str = "it monday"
+if(rating === 3){
+    console.log("You are population");
+}
+if(str.indexOf('a') != -1){
+    console.log("a is part of string!!");
+}
