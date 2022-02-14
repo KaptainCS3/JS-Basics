2c38bc9680210bffdca0765f20402b1f9ff5e792
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-14 11:23:54 +0100

    section done

---
+++ Colt_Steele_Udemy/JS_Decision_Making/elseIfCon.js
@@ -0,0 +1,10 @@
+let str = "KaptainCS3";
+if(str.indexOf("a") === 2){
+    console.log("index of a is 2");
+}
+else if(str.indexOf("c") != -1){
+    console.log("char found");
+}
+else if(str.indexOf("K") != 1){
+    console.warn("char 'K' found");
+}
