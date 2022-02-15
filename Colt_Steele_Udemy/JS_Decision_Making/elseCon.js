e7d4f57b88e48b6356956d604ab944a7f164223e
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-15 23:11:56 +0100

    Else lession commited

---
+++ Colt_Steele_Udemy/JS_Decision_Making/elseCon.js
@@ -0,0 +1,14 @@
+    //  done by KaptainCS3  15 /02/ 2022
+    const dayOfWeek = 'Friday';
+    if(dayOfWeek === 'Monday'){
+        console.log("UGHHH I lOVE MONDAYS!");
+    }
+    else if(dayOfWeek === 'Saturday!'){
+        console.log("YAY I HAVE SATURDAYS");
+    }
+    else if(dayOfWeek === 'Friday!'){
+        console.log("FRIDAY ARE DECENT, ESPECAILLY AFTER WORK!");
+    }
+    else{
+        console.log("NOT A GREAT DAY!")
+    }
