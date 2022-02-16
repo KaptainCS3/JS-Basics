c37db3cfb32f5c90d1ab5bdc870d7ff65b7f81b3
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-16 11:40:53 +0100

    task done

---
+++ Colt_Steele_Udemy/JS_Decision_Making/true&falseValue.js
@@ -0,0 +1,9 @@
+    //done by KaptainCS3    16 /02/ 2022
+
+const userInput = prompt("Enter user name");
+if(userInput){
+    console.log("truthy value!!");
+}
+else{
+    console.log("falsy value!!");
+}
