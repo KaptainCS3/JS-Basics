e289fbb5101aceb870f71a667006c832e8f1c84b
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-17 14:08:02 +0100

    <type>[array DS] <description> [Crucial concept to pull specific data from array]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/accessArray.js
@@ -0,0 +1,10 @@
+//  done by KaptainCS3  17 /02/ 2022
+// accessing array using [] notation
+// accessing el with unknown index will return 'undefine' because the is no element at that particular index
+const elAnimals = ['Dog', 'Chat', 'Monkey', 'Lion', 'Leopard'];
+
+//with the general concept of accessing el in arrays using index 
+// with index starting at 0 and length is numEl - 1.
+elAnimals[3] // will return the arrayEl 'Lion'
+//print the result to the console
+console.log(elAnimals[3]);
