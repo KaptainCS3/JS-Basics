679bf98ed11b34f163d8b1c6785338dd99a4e51d
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-17 14:39:49 +0100

    <type>[Array Methods] <description> [Insert el at the end of the array]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/ArrayMethods/push.js
@@ -0,0 +1,9 @@
+// done by KaptainCS3   17 /02/ 2022
+
+//Crucial concept working with javascript arrays
+
+//built in method for array 'push' insert el at the end of the array
+
+const dayOfWeek = ['Monday', 'Tuesday'];
+dayOfWeek.push('Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');    //updates array content and size && and also take multi values
+console.warn(dayOfWeek);    // ! note el are inserted at the end of the array !!
