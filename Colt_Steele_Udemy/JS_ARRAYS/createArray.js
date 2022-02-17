e6ee8b1ff3bfec82c7257dd22b195df0593794ea
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-17 11:01:04 +0100

    <type>[array DS] <description> [create js array]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/createArray.js
@@ -0,0 +1,20 @@
+// done by KaptainCS3   17 /02/ 2022
+    /* in js array can store a character,number, object where as in 
+    c programming array are limited to store data of thesame type */
+
+
+    //declare an empty array and initialising
+    
+    let student = [];
+
+    //array of strings
+    
+    let colors = ['Red', 'Orange', 'Blue'];
+
+    //array of numbers
+    
+    let lottoNums = [19, 22, 50, 11, 4];
+
+    //Mixed array
+
+    let stuff = [true, 80, 'Dogs', undefined];
