a386e9ab97ce834c4f5a0b7b7ff775c7796a92d6
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-17 14:16:04 +0100

    <type>[array DS] <description> [Modify array size and content]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/modifyArray.js
@@ -0,0 +1,9 @@
+//  done by KaptainCS3  17 /02/ 2022
+// alternative for add elements to array using [] notation
+
+const names = ['Leonard', 'Esther', 'Martha', 'Dianna', 'Cynthia'];
+
+//adding el to name array
+ names[5] = 'John'; // insert 'John' at point 5 in names array
+ names[6] = 'Alex'; // insert 'Alex' at point 6 in names array
+ console.log(names); // modified Array with john & alex added. 
