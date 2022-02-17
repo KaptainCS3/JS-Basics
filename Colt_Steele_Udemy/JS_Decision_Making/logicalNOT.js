8b4fa8846cad755a3453908581445945fa67ed76
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-17 10:24:07 +0100

    <type> []: <description> [getting a mastery of not operator]

---
+++ Colt_Steele_Udemy/JS_Decision_Making/logicalNOT.js
@@ -0,0 +1,18 @@
+//  done by KaptainCS3  17 /02/ 2022
+
+// ! use for negating expression true to false and vice versal
+
+let result = null;
+let firstName = prompt("enter first name");
+if(!firstName){
+    firstName = prompt("Try Again!!!");
+}
+else{
+    console.log("Valid input!!");
+}
+if(!result){
+    console.log("negation is true");
+}
+else{
+    console.log("negation is false");
+}
