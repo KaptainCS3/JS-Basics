bbe7bb1f2af6f02871a6a0e046d914a0059abc27
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-17 10:41:25 +0100

    <type>[Decisions] <description> [Alternative for if else condition.Section complete]

---
+++ Colt_Steele_Udemy/JS_Decision_Making/switchCase.js
@@ -0,0 +1,29 @@
+//  done by KaptainCS3  17 /02/ 2022
+// switch case alternative for writing conditions statements
+// break keyword to stop the execution of the case else it will execute all case for a given value
+const day = 4;
+switch(day){
+    case 1:{
+        console.log("MONDAY");
+        break;
+    }
+    case 2:{
+        console.log("TUESDAY");
+        break;
+    }
+    case 3:{
+        console.log("WEDNESDAY");
+        break;
+    }
+    case 4:{
+        console.log("THURSDAY");
+        break;
+    }
+    case 5:{
+        console.log("FRIDAY");
+        break;
+    }
+    default:{
+        console.log("Either SATURDAY OR SUNDAY!")
+    }
+}
