e144db896a8e35e18f4c9ade080708a512c6fe84
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-17 14:17:43 +0100

    <type>[array DS] <description> [Nothing important to commit]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/updatingArray.js
@@ -0,0 +1,5 @@
+//  done by KaptainCS3  17 /02/ 2022
+//  updating aelements of an array will create a new version of the array with the corresponding updates
+const colors = ['rad', 'blue', 'black', 'green'];
+colors[0] = 'red';
+console.log(colors);
