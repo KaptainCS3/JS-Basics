a1f2fb868fabd0c587b81b68fb4bc87071ba843e
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-18 13:58:28 +0100

    <type>[Conditionals and Loop] <description> [Practice exercises]

---
+++ Colt_Steele_Udemy/JS_Decision_Making/practiceExercise/oddEven.js
@@ -0,0 +1,9 @@
+let maxNum = 15;
+for(let i = 1; i <= maxNum; i++){
+    if(i % 2 != 0){
+        console.log(`number ${i} is Odd`);
+    }
+    else{
+        console.log(`number ${i} is Even`);
+    }
+}
