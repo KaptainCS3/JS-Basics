6a1f68e13bc193ec6b8db3dc040a6e5f5ee13c39
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-18 14:18:27 +0100

    <type>[Array DS] <description> [Unshift insert el at begining]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/ArrayMethods/unshift.js
@@ -0,0 +1,10 @@
+// done by KaptainCS3   18 /02/ 2022
+
+//Crucial concept working with javascript arrays
+
+//built in method for array 'unshift' insert el at the begining of the array
+
+const dayOfWeek = ['Wednesday', 'Tuesday', 'Monday'];
+dayOfWeek.unshift('Sunday', 'Saturday', 'Friday', 'Thursday',)    //updates array content and size && and also take multi values
+console.warn(dayOfWeek);    //! note el are inserted at the begining of the array !!
+// return 'Sunday', 'Saturday', 'Friday', 'Thursday','Wednesday', 'Tuesday', 'Monday'
