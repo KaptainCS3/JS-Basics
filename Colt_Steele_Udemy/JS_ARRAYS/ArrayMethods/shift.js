bdd9a8864146f84cddd7dd80b7fe214e7588a41e
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-18 14:19:00 +0100

    <type>[Array DS] <description> [Shift remove el at begining]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/ArrayMethods/shift.js
@@ -0,0 +1,14 @@
+// done by KaptainCS3   18 /02/ 2022
+
+//Crucial concept working with javascript arrays
+
+//built in method for array 'shift' remove el at the begining of the array
+
+const dayOfWeek = ['Sunday', 'Saturday', 'Friday', 'Thursday', 'Wednesday', 'Tuesday', 'Monday'];
+dayOfWeek.unshift()    //updates array content and size && and also take multi values
+dayOfWeek.unshift()    //updates array content and size && and also take multi values
+dayOfWeek.unshift()    //updates array content and size && and also take multi values
+dayOfWeek.unshift()    //updates array content and size && and also take multi values
+dayOfWeek.unshift()    //updates array content and size && and also take multi values
+console.warn(dayOfWeek);    //! note el are removed from the begining of the array !!
+// return 'Tuesday', 'Monday'
