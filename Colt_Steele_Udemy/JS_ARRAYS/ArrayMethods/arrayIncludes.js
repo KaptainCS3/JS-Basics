23111d516165e6f92f218c0af9a29681325f365f
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-19 11:29:47 +0100

    <type>[Array Method] <description> [Boolean method for array entries]

---
+++ Colt_Steele_Udemy/JS_ARRAYS/ArrayMethods/arrayIncludes.js
@@ -0,0 +1,15 @@
+//  done by KaptainCS3  19 /02/ 2022
+
+            /* Method determines whether an array includes a certain
+             value among its entries. Method returns a boolean*/
+
+const array1 = [1, 2, 3];
+console.log(array1.includes(2));
+// expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+console.log(pets.includes('cat'));
+// expected output: true
+
+console.log(pets.includes('Bat'));
+// expected output: false
