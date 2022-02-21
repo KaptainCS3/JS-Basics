c93ec6bf4c080c14dc7490002f6308776febbcf9
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-21 13:41:49 +0100

    <type>[Obj DS] <description> [Creating js object. Storing data in a sequential other]

---
+++ Colt_Steele_Udemy/JS_OBJECTSLITERAL/createObject.js
@@ -0,0 +1,27 @@
+/*  Author: KaptainCS3 
+ *  Description: Object Literal Intro
+ *  Date: 21th February 2022
+ */
+
+//modeling a person
+
+const person = {
+    firstName : 'Mbi Enow',
+    lastName : 'Leonard Appelgryn',
+    age : 20,
+    height : 1.81,
+    nationality : 'Cameroonian',
+    maritalStatus : 'Single'
+};
+
+//modeling a blog post comment
+
+let comment = {
+    userName : 'KaptainCS3',
+    downVotes : 5,
+    upVotes : 250,
+    netScore : 200,
+    commentText : 'React, Sass and Node is awesome',
+    tags : ['#Yellowflash', '#Turkson', '#Durel'],
+    isAdmin : true
+};
