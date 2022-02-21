f662241a4be20103c7ef055bd39245b482e33796
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-21 21:12:01 +0100

    <type>[Obj DS] <description> [Arrays + Objects, Objects + Arrays]

---
+++ Colt_Steele_Udemy/JS_OBJECTSLITERAL/nestArray_Object.js
@@ -0,0 +1,35 @@
+/*  Author: KaptainCS3 
+ *  Description: Object Literal nesting array and object
+ *  Date: 21th February 2022
+ */
+
+// shoppingCart arrayObject
+const shoppingCart = [
+    {
+        product : "Jenga Classic",
+        price : 6.88,
+        quantity : 1
+    },
+    {
+        product : "Echo Dot",
+        price : 29.99,
+        quantity : 3
+    },
+    {
+        product : "Fire Stick",
+        price : 39.99,
+        quantity : 2
+    }
+];
+
+// student objectArray
+
+const student = {
+    firstName : "David",
+    lastName : "Jones",
+    strenghts : ["Music", "Art"],
+    exams :{
+        midterm : 92,
+        final : 88
+    }
+};
