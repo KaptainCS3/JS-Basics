37d7aaac3b6d9dde48c89553fbd7211cc57a39fa
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-21 21:10:19 +0100

    <type>[Obj DS] <description> []

---
+++ Colt_Steele_Udemy/JS_OBJECTSLITERAL/modifyingObj.js
@@ -0,0 +1,19 @@
+/*  Author: KaptainCS3 
+ *  Description: Object Literal data access
+ *  Date: 21th February 2022
+ */
+
+
+const midterms = {
+        thomas : 56,
+        terry : 68,
+        Leon : 80
+};
+
+//updating midtermsObj
+
+// using "." notation "[]" notation
+
+midterms.daysi = 79;
+midterms["larry"] = 50;
+console.log(midterms);
