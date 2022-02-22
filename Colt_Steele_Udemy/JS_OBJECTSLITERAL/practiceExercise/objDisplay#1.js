c8dc8f77fb2db89c3d36c94e05c94436b77ef1fa
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-22 11:25:53 +0100

    <type>[Practice Exercise] <description> [Display object data]

---
+++ Colt_Steele_Udemy/JS_OBJECTSLITERAL/practiceExercise/objDisplay#1.js
@@ -0,0 +1,17 @@
+/*  Author: KaptainCS3 
+ *  Description: Object Literal practice exercise
+ *  Date: 22th February 2022
+ */
+
+// reference w3sources.com/javascript-exercises/javascript-object-exercises.php
+
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12
+};
+
+// list name, sclass, rollno
+
+console.log(student.name, student.sclass, student.rollno);
+// expected output : David Rayy, VI, 12
