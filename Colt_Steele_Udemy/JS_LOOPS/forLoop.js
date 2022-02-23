1fa2ca72ec312e0343f88bfcd028e1d7dda2903a
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-23 12:20:40 +0100

    <type>[Automation on Code] <description> [regular loop type]

---
+++ Colt_Steele_Udemy/JS_LOOPS/forLoop.js
@@ -0,0 +1,13 @@
+/*  Author: KaptainCS3 
+ *  Description: For Loop
+ *  Date: 23th February 2022
+ */
+
+// repeating stuff with code
+
+for(let i = 1; i < 10; i++){
+    console.log(i);
+}
+console.log("\n");
+for(let j = 1; j < 10; j++)
+console.log("Hey today is Meeting!!");
