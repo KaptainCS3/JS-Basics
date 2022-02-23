5480a746c5c5bd14dfda1aba46beb31994f809d3
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-23 12:20:14 +0100

    <type>[Automation on Code] <description> [regular loop type]

---
+++ Colt_Steele_Udemy/JS_LOOPS/whileLoop.js
@@ -0,0 +1,15 @@
+/*  Author: KaptainCS3 
+ *  Description: While Loops
+ *  Date: 23th February 2022
+ */
+
+// countdown with while loop
+
+let x = 100;
+while(x != 0){
+    console.log(x);
+    if(x == 1){
+        console.log("CountDown complete!! switch to the next task");
+        }
+    x--;
+}
