dd963a46936c18fcba431e76492362c85ae48c74
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-23 12:36:40 +0100

    <type>[Practice exercise] <description> [insert el to array using for loop and use for of loop to iterate over array]

---
+++ Colt_Steele_Udemy/JS_LOOPS/Practice Exercises/arrayLoop.js
@@ -0,0 +1,13 @@
+/*  Author: KaptainCS3 
+ *  Description: practice exercise
+ *  Date: 23th February 2022
+ */
+
+// iterate using index
+let numArray = [];
+for(let i = 1; i <= 100; i++){
+    numArray.push(i);    // insert el to array using loop and array method
+}
+for(let j = 0; j < numArray.length; j++)
+    console.log(numArray[j]);
+
