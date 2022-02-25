f3765d463380b40737eb64e94dcbafbaa5452045
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-25 10:20:39 +0100

    <type>[Functions] <description> [Passing values to functions]

---
+++ Colt_Steele_Udemy/JS_FUNCTIONS/argumentsIntro.js
@@ -0,0 +1,11 @@
+/*  Author: KaptainCS3 
+ *  Description: Argument Intro
+ *  Date: 25th February 2022
+ */
+// args input to functions and methods
+
+function showMessage(from,text){
+    alert(`${from} : ${text}`);
+}
+showMessage('Ann', 'Hello!');
+showMessage('Ann', "What's up?");
