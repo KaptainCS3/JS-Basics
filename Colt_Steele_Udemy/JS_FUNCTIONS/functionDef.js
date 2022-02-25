173f48ac4025efef5d84bd2fcfec724161eae281
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-25 10:21:44 +0100

    <type>[Functions] <description> [Defining function and executing]

---
+++ Colt_Steele_Udemy/JS_FUNCTIONS/functionDef.js
@@ -0,0 +1,11 @@
+/*  Author: KaptainCS3 
+ *  Description: Function Definition
+ *  Date: 25th February 2022
+ */
+
+function grumpus(){
+    console.log('ugh....you again...');
+    console.log('for the last time....');
+    console.log('LEAVE ME ALONE!!!!');
+}
+grumpus();
