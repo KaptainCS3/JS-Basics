6212d23c91d38259327ee0d67cd19a592abc6c80
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-28 21:31:42 +0100

    <type>[Function scope] <description>[Variable visibility]

---
+++ Colt_Steele_Udemy/LEVELING_UP_FUNCTIONS/functionScope.js
@@ -0,0 +1,35 @@
+/*  Author: KaptainCS3 
+ *  Description: Argument Intro
+ *  Date: 25th February 2022
+ */
+
+// example#1
+
+function collectEggs(){
+    let totalEggs = 6;
+}
+collectEggs();
+console.log(totalEggs);
+            /*       expected output: return error because function scope 
+            variable declare with let keyword can! be accessed out of the function      */
+
+// example#2
+
+function helpMe(){
+    let msg = "I'm on fire!!";
+    msg;    //"I'm on fire";
+}
+helpMe()
+msg;    //! DEFINED!
+// msg is scoped to the helpMe function
+
+// example#3
+
+let bird = 'mandarin duck';
+function birdWatch(){
+    let bird = 'golden pheasant';
+    bird;   //'golden pheasant'
+            /* in this case if to variable have thesame name the function 
+            scoped variable wins out else the global scope variable wins out */
+}
+bird;   //'mandarin duck'
